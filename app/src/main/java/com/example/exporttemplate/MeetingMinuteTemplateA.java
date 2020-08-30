1876c6d20bfe80873dcec108cce27a2a645b8f50
Hye Wook Jung <jung_hye_wook@tech.gov.sg>
2020-08-30 20:07:11 +0800

    formatting with spans

--- app/src/main/java/com/example/exporttemplate/MeetingMinuteTemplateA.java
+++ app/src/main/java/com/example/exporttemplate/MeetingMinuteTemplateA.java
@@ -2,27 +2,47 @@ package com.example.exporttemplate;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.ContextWrapper;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.pdf.PdfDocument;
 import android.graphics.pdf.PdfDocument.Page;
 import android.graphics.pdf.PdfDocument.PageInfo;
-import android.util.Log;
+import android.os.Build;
+import android.text.Html;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.TextUtils;
+import android.text.style.BulletSpan;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
+import android.text.style.UnderlineSpan;
+import android.util.DisplayMetrics;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Calendar;
+import java.util.Date;
+
+@RequiresApi(api = Build.VERSION_CODES.O)
 
 public class MeetingMinuteTemplateA {
-    private Context context = null;
+    Context context = null;
+    int textViewId;
+    OutputStream os;
+    int height;
+    int width;
 
-    private int pageWidth = 300;
-    private int pageHeight = 300;
-
-    private int textViewId;
-
-    private OutputStream os;
+    TextView meetingMinuteContent;
 
     public MeetingMinuteTemplateA(Context context, int textViewId){
         this.context = context;
@@ -33,25 +53,40 @@ public class MeetingMinuteTemplateA {
         if(new File(path).exists())
             new File(path).delete();
 
+        getDisplayMetrics();
+
         PdfDocument document = new PdfDocument();
-        PageInfo pageInfo = new PageInfo.Builder(pageWidth, pageHeight, 1).create();
+        PageInfo pageInfo = new PageInfo.Builder(width, height, 1).create();
         Page page = document.startPage(pageInfo);
 
-        Log.e("error", "hello");
-        Log.e("error", path);
-        Log.e("error", Integer.toString(textViewId));
+        meetingMinuteContent = (TextView)getActivity(context).findViewById(textViewId);
 
-        LinearLayout layout = new LinearLayout(context);
+        SpannableStringBuilder meetingMinuteContentText = new SpannableStringBuilder();
 
-        View content = ((Activity) context).findViewById(textViewId);
-        content.measure(800, 480);
-        content.layout(100, 100, 300, 200);
+        meetingMinuteContentText.append(addTitle("title"));
 
-//        content.layout(0, 0, pageWidth,pageHeight);
-        Log.i("draw view", " content size: "+content.getWidth()+" / "+content.getHeight());
+        meetingMinuteContentText.append(addLineBreaker(2));
 
+        meetingMinuteContentText.append(addHeader("header"));
+        meetingMinuteContentText.append(addSubHeader("subHeader"));
+
+        meetingMinuteContentText.append(addBulletLine("point1"));
+        meetingMinuteContentText.append(addBulletLine("point2"));
+        meetingMinuteContentText.append(addBulletLine("point3"));
+
+        meetingMinuteContentText.append(addLineBreaker(1));
+
+        meetingMinuteContentText.append(addHeader("header"));
+        meetingMinuteContentText.append(addSubHeader("subHeader"));
+
+        meetingMinuteContentText.append(addBulletLine("point1"));
+        meetingMinuteContentText.append(addBulletLine("point2"));
+        meetingMinuteContentText.append(addBulletLine("point3"));
+
+        meetingMinuteContent.setText(meetingMinuteContentText);
+
+        View content = ((Activity) context).findViewById(textViewId); //Log.i("draw view", " content size: "+content.getWidth()+" / "+content.getHeight());
         content.draw(page.getCanvas());
-
         document.finishPage(page);
 
         try {
@@ -68,31 +103,102 @@ public class MeetingMinuteTemplateA {
         }
     }
 
-//    private void addNewItemWithLeftAndRight(Document document, String textLeft, String textRight, Font textLeftFont, Font textRightFont) throws DocumentException{
-//        Chunk chunkTextLeft = new Chunk(textLeft, textLeftFont);
-//        Chunk chunkTextRight = new Chunk(textLeft, textRightFont);
-//        Paragraph p = new Paragraph(chunkTextLeft);
-//        p.add(new Chunk(new VerticalPositionMark()));
-//        p.add(chunkTextRight);
-//        document.add(p);
-//    }
-//
-//    private void addLineSeperator(Document document) throws DocumentException  {
-//        LineSeparator lineSeperator = new LineSeparator();
-//        lineSeperator.setLineColor(new BaseColor(0,0,0,68));
-//        addLineSpace(document);
-//        document.add(new Chunk(lineSeperator));
-//        addLineSpace(document);
-//    }
-//
-//    private void addLineSpace(Document document) throws DocumentException {
-//        document.add(new Paragraph(""));
-//    }
-//
-//    private void addNewItem(Document document, String text, int align, Font font) throws DocumentException {
-//        Chunk chunk = new Chunk(text, font);
-//        Paragraph paragraph = new Paragraph(chunk);
-//        paragraph.setAlignment((align));
-//        document.add(paragraph);
-//    }
+    private Activity getActivity(Context context)
+    {
+        if (context == null)
+        {
+            return null;
+        }
+        else if (context instanceof ContextWrapper)
+        {
+            if (context instanceof Activity)
+            {
+                return (Activity) context;
+            }
+            else
+            {
+                return getActivity(((ContextWrapper) context).getBaseContext());
+            }
+        }
+
+        return null;
+    }
+
+    private void getDisplayMetrics(){
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getActivity(context).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        height = displayMetrics.heightPixels;
+        width = displayMetrics.widthPixels;
+    }
+
+    private SpannableString addTitle(CharSequence text){
+        //title of meeting
+        //date-time of meeting
+        if (TextUtils.isEmpty(text)) {
+            return null;
+        }
+
+        Date currentTime = Calendar.getInstance().getTime();
+
+        SpannableString ss = new SpannableString(text+"\n"+currentTime+"\n");
+        ss.setSpan(new RelativeSizeSpan(2f),0,text.length(),Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(new UnderlineSpan(), 0, text.length(), 0);
+
+        return ss;
+    }
+
+    private SpannableString addHeader(CharSequence text){
+        if (TextUtils.isEmpty(text)) {
+            return null;
+        }
+        StyleSpan styleSpan = new StyleSpan(android.graphics.Typeface.BOLD);
+
+        //spannableString.setSpan(new RelativeSizeSpan(2f),0,4,Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        SpannableString ss = new SpannableString(text+"\n");
+        ss.setSpan(new RelativeSizeSpan(3f),0,text.length(),Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(styleSpan, 0, ss.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(new ForegroundColorSpan(Color.RED), 0,  ss.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        return ss;
+    }
+
+    private SpannableString addSubHeader(CharSequence text){
+        if (TextUtils.isEmpty(text)) {
+            return null;
+        }
+        SpannableString ss = new SpannableString(text+"\n");
+        ss.setSpan(new RelativeSizeSpan(2f),0,text.length(),Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        return ss;
+    }
+
+    private SpannableString addBulletLine(CharSequence text){
+        if (TextUtils.isEmpty(text)) {
+            return null;
+        }
+
+        SpannableString ss = new SpannableString(text+"\n");
+        ss.setSpan(new BulletSpan(10), 0, text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        //        SpannableString bulletss = new SpannableString("My text \nbullet one\nbullet two\n");
+//        bulletss.setSpan(new BulletSpan(10), 9, 10, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+//        bulletss.setSpan(new BulletSpan(10), 20, 21, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+//        spannableString.append(bulletss);
+
+        return ss;
+    }
+
+    private StringBuilder addLineBreaker(int num){
+        if(num <= 0) return null;
+        StringBuilder s = new StringBuilder();
+        for(int i=0; i<=num;i++){
+            s.append("\n");
+        }
+
+        return s;
+    }
+
+    private void drawSeperator(int x, int y){
+
+    }
 }
