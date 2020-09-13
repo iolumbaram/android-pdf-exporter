b350a5f2d6a491fa3a56c809a119c75231addacb
Hye Wook Jung <jung_hye_wook@tech.gov.sg>
2020-09-13 23:22:56 +0800

    scaling banner image, refactor page break

--- app/src/main/java/com/example/exporttemplate/MeetingMinuteTemplateA.java
+++ app/src/main/java/com/example/exporttemplate/MeetingMinuteTemplateA.java
@@ -3,7 +3,10 @@ package com.example.exporttemplate;
 import android.app.Activity;
 import android.content.Context;
 import android.content.ContextWrapper;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.text.Spannable;
@@ -18,6 +21,7 @@ import android.text.style.ImageSpan;
 import android.text.style.StyleSpan;
 import android.text.style.UnderlineSpan;
 import android.util.DisplayMetrics;
+import android.util.Log;
 
 import androidx.annotation.RequiresApi;
 
@@ -27,6 +31,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 import static com.example.exporttemplate.R.drawable.banner_test;
 
@@ -39,7 +44,7 @@ public class MeetingMinuteTemplateA {
     int height;
     int width;
 
-    private int maxPageLines = 13; //every 27th lines create a new page
+    private int maxPageLines = 12; //every 27th lines create a new page
 
     public MeetingMinuteTemplateA(Context context, int textViewId, String path){
         this.context = context;
@@ -70,73 +75,155 @@ public class MeetingMinuteTemplateA {
             ss.clear();
 
             ss.append(addHeader("Notes"));
-            int sumChunkLines = 0;
+            ArrayList<SpannableString> chunksAll = new ArrayList<SpannableString>();
             for(int i=0;i<paras.size();i++){
                 ArrayList<SpannableString> chunks = createChuncks(paras.get(i));
-                int len = chunks.size();
-                int chunkCursor = 0;
-
-                while(chunkCursor < len && chunkCursor < maxPageLines){
-                    ss.append(chunks.get(chunkCursor));
-                    sumChunkLines++;
-                    chunkCursor++;
-                }
-
-                if(sumChunkLines >= maxPageLines){
-                    SpannableStringBuilder formattedPage = new SpannableStringBuilder();
-                    Spanned formattedLine = SpannableString.valueOf(ss);
-                    formattedPage.append(formattedLine);
-                    templateContent.add(formattedPage);
-
-                    ss.delete(0, ss.length());
-
-                    int remaingLineCountCursor = chunkCursor;
-                    while(remaingLineCountCursor < len){
-                        ss.append(chunks.get(remaingLineCountCursor));
-                        remaingLineCountCursor++;
-                    }
-
-                    sumChunkLines = 0;
-                }
-
-                if(i == paras.size()-1 && chunkCursor <= maxPageLines){
-                    SpannableStringBuilder formattedPage = new SpannableStringBuilder();
-                    ss.delete(0, ss.length());
-
-                    int noResidualLines = len-chunkCursor;
-
-                    if(noResidualLines > maxPageLines){
-                        for(int j=chunkCursor; j<chunkCursor+maxPageLines; j++){
-                            ss.append(chunks.get(j));
-                        }
-                        Spanned formattedLine = SpannableString.valueOf(ss);
-                        formattedPage.append(formattedLine);
-                        templateContent.add(formattedPage);
-
-                        ss.delete(0, ss.length());
-
-                        for(int k=len-maxPageLines; k<len; k++){
-                            ss.append(chunks.get(k));
-                        }
-
-                        SpannableStringBuilder formattedPage2 = new SpannableStringBuilder();
-                        Spanned formattedLine2 = SpannableString.valueOf(ss);
-                        formattedPage2.append(SpannableString.valueOf(formattedLine2));
-                        templateContent.add(formattedPage2);
-                    }
-                    else{
-                        for(int j=chunkCursor; j<len; j++){
-                            ss.append(chunks.get(j));
-                        }
-                        Spanned formattedLine = SpannableString.valueOf(ss);
-                        formattedPage.append(formattedLine);
-                        templateContent.add(formattedPage);
-                    }
+                for(int j=0; j<chunks.size();j++){
+                    chunksAll.add(chunks.get(j));
                 }
             }
+
+            ArrayList<List<SpannableString>> split = chunkArrayList(chunksAll,maxPageLines);
+            for(int j=0; j<split.size();j++){
+                for(int k=0; k<split.get(j).size();k++){
+                    ss.append(split.get(j).get(k));
+                }
+                SpannableStringBuilder formattedPage = new SpannableStringBuilder();
+                Spanned formattedLine = SpannableString.valueOf(ss);
+                formattedPage.append(formattedLine);
+                templateContent.add(formattedPage);
+                ss.clear();
+            }
+
             return templateContent;
         }
         return null;
+    }
+
+//    Set the text size to size physical pixels, or to size device-independent pixels if dip is true.
+//    a4 - 3508 x 2480 size in pixel 300 dpi
+    private SpannableString addTitle(CharSequence text){
+        if (TextUtils.isEmpty(text)) {
+            return null;
+        }
+        SpannableStringBuilder sb = new SpannableStringBuilder();
+        int absoluteSizeSpan = 14;
+
+        String imagePlaceHolder = "1";
+        SpannableString imageSS = new SpannableString(imagePlaceHolder);
+        Bitmap bm = BitmapFactory.decodeResource(context.getResources(), banner_test);
+        Bitmap resized = Bitmap.createScaledBitmap(bm,(int)(bm.getWidth()*0.35), (int)(bm.getHeight()*0.75), true);
+        Drawable d = new BitmapDrawable(context.getResources(), resized);
+        d.setBounds(0, 0, d.getIntrinsicWidth(), d.getIntrinsicHeight());
+        ImageSpan span = new ImageSpan(d, ImageSpan.ALIGN_BASELINE);
+        imageSS.setSpan(span, 0, imagePlaceHolder.length(), Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
+        sb.append(imageSS);
+
+        sb.append("\n");
+
+        SpannableString titleText = new SpannableString("\r"+text+"\n");
+        titleText.setSpan(new AbsoluteSizeSpan(absoluteSizeSpan, true), 0, titleText.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        int brown = Color.parseColor("#695d46");
+        titleText.setSpan(new ForegroundColorSpan(brown), 0,  titleText.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        sb.append(titleText);
+
+        Date currentTime = Calendar.getInstance().getTime();
+        SpannableString dateTimeText = new SpannableString("\r"+currentTime+"\n");
+        dateTimeText.setSpan(new AbsoluteSizeSpan(9, true), 0, dateTimeText.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        sb.append(dateTimeText);
+
+        SpannableString seperator = new SpannableString("\r"+"       \n");
+        //ss.setSpan(new RelativeSizeSpan(2f),0,text.length(),Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        seperator.setSpan(new UnderlineSpan(), 1, seperator.length(), 0);
+        sb.append(seperator);
+
+        return  SpannableString.valueOf(sb);
+    }
+
+    private SpannableString addAttendees(ArrayList<String> attendeesList){
+        if (attendeesList.isEmpty()) {
+            return null;
+        }
+        SpannableStringBuilder sb = new SpannableStringBuilder();
+        int absoluteSizeSpan = 14;
+
+        StyleSpan styleSpan = new StyleSpan(android.graphics.Typeface.BOLD);
+
+        String headerText = "Attendees";
+
+        SpannableString ss = new SpannableString("\r"+headerText+"\n");
+        ss.setSpan(new LineHeightSetter(20), 0, headerText.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(new AbsoluteSizeSpan(absoluteSizeSpan, true), 0, headerText.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        //ss.setSpan(new RelativeSizeSpan(3f),0,text.length(),Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(styleSpan, 0, ss.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        int orange = Color.parseColor("#ff5e0e");
+        ss.setSpan(new ForegroundColorSpan(orange), 0,  ss.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        sb.append(ss);
+
+        for(int i=0; i<attendeesList.size(); i++){
+            SpannableString attendee = new SpannableString("\r\r"+attendeesList.get(i) +"\n");
+            attendee.setSpan(new LineHeightSetter(10), 0,attendee.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            attendee.setSpan(new AbsoluteSizeSpan(8, true), 0, attendee.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            //ss.setSpan(new RelativeSizeSpan(3f),0,text.length(),Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            attendee.setSpan(styleSpan, 0, attendee.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            sb.append(attendee);
+        }
+
+        return  SpannableString.valueOf(sb);
+    }
+
+    private SpannableString addHeader(CharSequence text){
+        if (TextUtils.isEmpty(text)) {
+            return null;
+        }
+        int absoluteSizeSpan = 14;
+
+        StyleSpan styleSpan = new StyleSpan(android.graphics.Typeface.BOLD);
+        SpannableString ss = new SpannableString("\r"+text+"\n");
+        //ss.setSpan(new RelativeSizeSpan(2f),0,text.length(),Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(new LineHeightSetter(10), 0,text.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(new AbsoluteSizeSpan(absoluteSizeSpan, true), 0, text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(styleSpan, 0, ss.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        int orange = Color.parseColor("#ff5e0e");
+        ss.setSpan(new ForegroundColorSpan(orange), 0,  ss.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        return ss;
+    }
+
+    private SpannableString addBulletLine(CharSequence text){
+        if (TextUtils.isEmpty(text)) {
+            return null;
+        }
+        int absoluteSizeSpan = 8;
+
+        SpannableString ss = new SpannableString(text);
+        ss.setSpan(new BulletSpan(5), 0, text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(new AbsoluteSizeSpan(absoluteSizeSpan, true), 0, text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        return ss;
+    }
+
+    private SpannableString addTab(CharSequence text){
+        if (TextUtils.isEmpty(text)) {
+            return null;
+        }
+        int absoluteSizeSpan = 8;
+
+        SpannableString ss = new SpannableString("\r\r\r\r"+text);
+        ss.setSpan(new AbsoluteSizeSpan(absoluteSizeSpan, true), 0, text.length()+3, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        return ss;
+    }
+
+    private StringBuilder addLineBreaker(int num){
+        if(num <= 0) return null;
+        String space = "";
+        StringBuilder s = new StringBuilder();
+        SpannableString ss = new SpannableString(space);
+        ss.setSpan(new LineHeightSetter(20), 0, space.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        s.append(ss);
+//        s.append("\n");
+
+        return s;
     }
 
     private ArrayList<String> splitToPara(String text){
@@ -179,6 +266,23 @@ public class MeetingMinuteTemplateA {
         return false;
     }
 
+    private ArrayList<List<SpannableString>> chunkArrayList(ArrayList<SpannableString> arrayToChunk, int chunkSize) {
+        ArrayList<List<SpannableString>> chunkList = new ArrayList<>();
+        int guide = arrayToChunk.size();
+        int index = 0;
+        int cursor = chunkSize;
+        while (cursor < arrayToChunk.size()){
+            chunkList.add(arrayToChunk.subList(index, cursor));
+            guide = guide - chunkSize;
+            index = index + chunkSize;
+            cursor = cursor + chunkSize;
+        }
+        if (guide >0) {
+            chunkList.add(arrayToChunk.subList(index, index + guide));
+        }
+        return chunkList;
+    }
+
     private Activity getActivity(Context context)
     {
         if (context == null)
@@ -205,152 +309,6 @@ public class MeetingMinuteTemplateA {
         getActivity(context).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         height = displayMetrics.heightPixels;
         width = displayMetrics.widthPixels;
-    }
-
-//    Set the text size to size physical pixels, or to size device-independent pixels if dip is true.
-//    a4 - 3508 x 2480 size in pixel 300 dpi
-    private SpannableString addTitle(CharSequence text){
-        if (TextUtils.isEmpty(text)) {
-            return null;
-        }
-        SpannableStringBuilder sb = new SpannableStringBuilder();
-        int absoluteSizeSpan = 14;
-
-        String imagePlaceHolder = "1";
-        SpannableString imageSS = new SpannableString(imagePlaceHolder);
-        Drawable d = context.getDrawable(banner_test);
-        d.setBounds(0, 0, d.getIntrinsicWidth(), d.getIntrinsicHeight());
-        ImageSpan span = new ImageSpan(d, ImageSpan.ALIGN_BASELINE);
-        imageSS.setSpan(span, 0, imagePlaceHolder.length(), Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
-        sb.append(imageSS);
-
-        sb.append("\n");
-
-        SpannableString titleText = new SpannableString(text+"\n");
-        titleText.setSpan(new AbsoluteSizeSpan(absoluteSizeSpan, true), 0, titleText.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        int brown = Color.parseColor("#695d46");
-        titleText.setSpan(new ForegroundColorSpan(brown), 0,  titleText.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        sb.append(titleText);
-
-        Date currentTime = Calendar.getInstance().getTime();
-        SpannableString dateTimeText = new SpannableString(currentTime+"\n");
-        dateTimeText.setSpan(new AbsoluteSizeSpan(9, true), 0, dateTimeText.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        sb.append(dateTimeText);
-
-        SpannableString seperator = new SpannableString("       \n");
-        //ss.setSpan(new RelativeSizeSpan(2f),0,text.length(),Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        seperator.setSpan(new UnderlineSpan(), 0, seperator.length(), 0);
-        sb.append(seperator);
-
-//        InSpanLimit(absoluteSizeSpan);
-        return  SpannableString.valueOf(sb);
-    }
-
-    private SpannableString addAttendees(ArrayList<String> attendeesList){
-        if (attendeesList.isEmpty()) {
-            return null;
-        }
-        SpannableStringBuilder sb = new SpannableStringBuilder();
-        int absoluteSizeSpan = 14;
-
-        StyleSpan styleSpan = new StyleSpan(android.graphics.Typeface.BOLD);
-
-        String headerText = "Attendees";
-
-        SpannableString ss = new SpannableString(headerText+"\n");
-        ss.setSpan(new LineHeightSetter(20), 0, headerText.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        ss.setSpan(new AbsoluteSizeSpan(absoluteSizeSpan, true), 0, headerText.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        //ss.setSpan(new RelativeSizeSpan(3f),0,text.length(),Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        ss.setSpan(styleSpan, 0, ss.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        int orange = Color.parseColor("#ff5e0e");
-        ss.setSpan(new ForegroundColorSpan(orange), 0,  ss.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        sb.append(ss);
-
-        for(int i=0; i<attendeesList.size(); i++){
-            SpannableString attendee = new SpannableString(attendeesList.get(i) +"\n");
-            attendee.setSpan(new LineHeightSetter(10), 0,attendee.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-            attendee.setSpan(new AbsoluteSizeSpan(8, true), 0, attendee.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-            //ss.setSpan(new RelativeSizeSpan(3f),0,text.length(),Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-            attendee.setSpan(styleSpan, 0, attendee.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-            sb.append(attendee);
-        }
-
-//        InSpanLimit(absoluteSizeSpan);
-        return  SpannableString.valueOf(sb);
-    }
-
-    private SpannableString addHeader(CharSequence text){
-        if (TextUtils.isEmpty(text)) {
-            return null;
-        }
-        int absoluteSizeSpan = 14;
-
-        StyleSpan styleSpan = new StyleSpan(android.graphics.Typeface.BOLD);
-        SpannableString ss = new SpannableString(text+"\n");
-        //ss.setSpan(new RelativeSizeSpan(2f),0,text.length(),Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        ss.setSpan(new LineHeightSetter(10), 0,text.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        ss.setSpan(new AbsoluteSizeSpan(absoluteSizeSpan, true), 0, text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        ss.setSpan(styleSpan, 0, ss.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        int orange = Color.parseColor("#ff5e0e");
-        ss.setSpan(new ForegroundColorSpan(orange), 0,  ss.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-
-//        InSpanLimit(absoluteSizeSpan);
-        return ss;
-    }
-
-    private SpannableString addBulletLine(CharSequence text){
-        if (TextUtils.isEmpty(text)) {
-            return null;
-        }
-        int absoluteSizeSpan = 8;
-
-        SpannableString ss = new SpannableString(text);
-        ss.setSpan(new LineHeightSetter(10), 0,text.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        ss.setSpan(new BulletSpan(10), 0, text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        ss.setSpan(new AbsoluteSizeSpan(absoluteSizeSpan, true), 0, text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-//        InSpanLimit(absoluteSizeSpan);
-        return ss;
-    }
-
-    private SpannableString addTab(CharSequence text){
-        if (TextUtils.isEmpty(text)) {
-            return null;
-        }
-        //text = String.format("%1$-" + 1 + "s", text);
-        int absoluteSizeSpan = 8;
-
-        SpannableString ss = new SpannableString("\r\r\r"+text);
-        ss.setSpan(new LineHeightSetter(10), 0,text.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        ss.setSpan(new AbsoluteSizeSpan(absoluteSizeSpan, true), 0, text.length()+3, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-
-//        InSpanLimit(absoluteSizeSpan);
-        return ss;
-    }
-
-    private StringBuilder addLineBreaker(int num){
-        if(num <= 0) return null;
-        String space = "";
-        StringBuilder s = new StringBuilder();
-        SpannableString ss = new SpannableString(space);
-        ss.setSpan(new LineHeightSetter(20), 0, space.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        s.append(ss);
-//        s.append("\n");
-
-        return s;
-    }
-
-//    private boolean InSpanLimit(int spanCount){
-//        ssBuilderSpanSize += spanCount;
-//        if(ssBuilderSpanSize >= ssBuilderSpanSizeLimit) {
-//            SpannableStringBuilders[0] = meetingMinuteContentText;
-//            meetingMinuteContentText.clear();
-//            return false;
-//        }
-//        return true;
-//    }
-
-    private void drawSeperator(int x, int y){
-
     }
 }
 
